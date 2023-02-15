45780cf7badbbd6f4fff8f54731d69136c7a1d78
root <ahmadrathore@gmail.com>
2023-02-15 19:02:43 +0000

    Add functions (batch size, instanceGroup, optimization) for model config

--- simple_triton/submitter.py
+++ simple_triton/submitter.py
@@ -149,11 +149,11 @@ if __name__ == "__main__":
     batch_size = 1024
     dimension_input = 1024
     dimension_output = 1
-    count = 1 
-    kind="KIND_GPU" 
-    gpus= [ 0 ]
-    instance_config = {'count': count, 'kind': kind , 'gpus':gpus}
-    batch_config = "{\"max_batch_size\":\"2048\"}"
+    count = 1
+    kind = "KIND_GPU"
+    gpus = [0]
+    instance_config = {"count": count, "kind": kind, "gpus": gpus}
+    batch_config = '{"max_batch_size":"2048"}'
     optimization = '{"execution_accelerators":{"gpu_execution_accelerator" : [\
            {"name" : "tensorrt", "parameters": {"precision_mode": "FP16"}}]}}'
 
@@ -165,7 +165,9 @@ if __name__ == "__main__":
     except Exception as e:
         print("context creation failed: " + str(e), flush=True)
 
-    config_model_updated = model_update(batch_config, instance_config, optimization, client, model_name_test)
+    config_model_updated = model_update(
+        batch_config, instance_config, optimization, client, model_name_test
+    )
 
     # start timer
     start = time.time()
