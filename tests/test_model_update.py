8bb26a0ce2b623f6f553e460e964442ca64f9220
root <ahmadrathore@gmail.com>
2023-03-16 11:42:27 +0000

    add a function check_add_accelerator

--- tests/test_model_update.py
+++ tests/test_model_update.py
@@ -137,6 +137,18 @@ def gpu_accelerator_add(config, accelerator=gpu_accelerator_trt("FP16")):
     )
 
 
+def check_add_accelerator(accelerator=None):
+    if accelerator == "auto_mixed_precision":
+        if gpu_accelerator_status(config, accelerator) == False:
+            gpu_accelerator_add(config, gpu_accelerator_amp)
+    if accelerator == "tensorrt":
+        if gpu_accelerator_status(config, "tensorrt") == False:
+            gpu_accelerator_add(config, gpu_accelerator_trt("FP16"))
+    if accelerator == "tensorrt_FP32":
+        if gpu_accelerator_status(config, "tensorrt") == False:
+            gpu_accelerator_add(config, gpu_accelerator_trt("FP32"))
+
+
 def test_update_model_instance_gpu():
     """Evaluate instance group for  kind="gpu","""
     instance = instance_group(model_name, count=1, kind="gpu", gpus=[0])
@@ -162,8 +174,7 @@ def test_update_model_instance_cpu():
 def test_update_model_amp_False():
     """Evaluate remove amp if amp==False and amp is in current config"""
     # add amp if not already in the config
-    if gpu_accelerator_status(config, "auto_mixed_precision") == False:
-        gpu_accelerator_add(config, gpu_accelerator_amp)
+    check_add_accelerator("auto_mixed_precision")
 
     model_update(
         client, model_name, max_batch_size=None, instances=None, trt=None, amp=False
@@ -180,10 +191,9 @@ def test_update_model_amp_False():
 def test_update_model_trt_None():
     """Evaluate remove trt if trt is None and trt is in current config"""
     # add both trt and amp if not already in the config
-    if gpu_accelerator_status(config, "auto_mixed_precision") == False:
-        gpu_accelerator_add(config, gpu_accelerator_amp)
-    if gpu_accelerator_status(config, "tensorrt") == False:
-        gpu_accelerator_add(config, gpu_accelerator_trt("FP16"))
+
+    check_add_accelerator("auto_mixed_precision")
+    check_add_accelerator("tensorrt")
 
     model_update(
         client, model_name, max_batch_size=None, instances=None, trt=None, amp=False
@@ -200,8 +210,7 @@ def test_update_model_trt_None():
 def test_update_model_amp_True_trt_None():
     """Evaluate if amp is True, add only if trt is None"""
     # add trt if not already in the config
-    if gpu_accelerator_status(config, "tensorrt") == False:
-        gpu_accelerator_add(config, gpu_accelerator_trt("FP16"))
+    check_add_accelerator("tensorrt")
 
     model_update(
         client, model_name, max_batch_size=None, instances=None, trt=None, amp=True
@@ -218,8 +227,7 @@ def test_update_model_amp_True_trt_None():
 def test_update_model_trt_NotNone_FP16():
     """Evaluate if trt is not none add trt = "FP16", remove amp if necessary"""
     # add amp if not already in the config
-    if gpu_accelerator_status(config, "auto_mixed_precision") == False:
-        gpu_accelerator_add(config, gpu_accelerator_amp)
+    check_add_accelerator("auto_mixed_precision")
 
     model_update(
         client, model_name, max_batch_size=None, instances=None, trt="FP16", amp=False
@@ -236,8 +244,7 @@ def test_update_model_trt_NotNone_FP16():
 def test_update_model_trt_NotNone_FP32():
     """Evaluate if trt is not none add trt = "FP16", remove amp if necessary"""
     # add amp if not already in the config
-    if gpu_accelerator_status(config, "auto_mixed_precision") == False:
-        gpu_accelerator_add(config, gpu_accelerator_amp)
+    check_add_accelerator("auto_mixed_precision")
 
     model_update(
         client, model_name, max_batch_size=None, instances=None, trt="FP32", amp=False
