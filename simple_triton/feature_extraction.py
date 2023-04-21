9878b227b60ee2d100523961b345d1988937426f
Lee Cooper <cooperle@gmail.com>
2023-04-21 15:32:34 -0500

    Return failed jobs

--- simple_triton/feature_extraction.py
+++ simple_triton/feature_extraction.py
@@ -77,17 +77,24 @@ def histomics_stream_inference(
             output["times"]["qout_get"] = t_get
             batches.append(output)
 
-    # separate results
+    # successful results results
     features = [
-        [b["result"][i] for b in batches] for i in range(len(batches[0]["result"]))
+        [b["result"][i] for b in batches if b["success"]]
+        for i in range(len(batches[0]["result"]))
     ]
     tile_info = {
-        k: np.concatenate([b["metadata"][k] for b in batches])
+        k: np.concatenate([b["metadata"][k] for b in batches if b["success"]])
         for k in batches[0]["metadata"].keys()
     }
-    times = {k: [b["times"][k] for b in batches] for k in batches[0]["times"].keys()}
+    times = {
+        k: [b["times"][k] for b in batches if b["success"]]
+        for k in batches[0]["times"].keys()
+    }
 
-    return features, tile_info, times
+    # failures
+    failed = [b for b in batches if not b["success"]]
+
+    return features, tile_info, times, failed
 
 
 def feature_extractor(repository, model, name, t=(224, 224), pool="avg"):
