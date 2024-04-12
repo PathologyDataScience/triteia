77acd91f7b43d96c00c780d90ba324889652bf98
Lee Cooper <cooperle@gmail.com>
2024-04-11 22:59:44 -0500

    group-read shared memory tile iterator

--- simple_triton/tile_iterators.py
+++ simple_triton/tile_iterators.py
@@ -1,19 +1,18 @@
+from collections import deque
 from concurrent.futures import (
     ProcessPoolExecutor,
-    ThreadPoolExecutor,
-    ALL_COMPLETED,
-    FIRST_COMPLETED,
-    wait,
+    ALL_COMPLETED, 
+    wait
 )
-from copy import deepcopy
 import functools
+import histomics_stream as hs
 import large_image_source_tiff
+import math
 import multiprocessing.shared_memory
 import numpy as np
 import operator
 import os
 import PIL
-from queue import Queue
 
 
 class SharedNumpyArray:
@@ -23,15 +22,19 @@ class SharedNumpyArray:
         self.dtype = np.dtype(dtype)
         self.shm_size = functools.reduce(operator.mul, shape, 1) * self.dtype.itemsize
         self.shm = multiprocessing.shared_memory.SharedMemory(
-            create=True, size=self.shm_size
-        )
+            create=True, size=self.shm_size)
+        self.buf = np.ndarray(self.shape, dtype=self.dtype, buffer=self.shm.buf)
         self.created = True
 
+    def insert(self, arr, i):
+        """Insert a batch dimension slice."""
+        self.buf[i] = arr
+
     def copy(self, arr):
-        self.shape = arr.shape
+        self.shape = arry.shape
         self.buf = np.ndarray(self.shape, dtype=self.dtype, buffer=self.shm.buf)
         self.buf[:] = arr[:]
-
+    
     def tobytes(self):
         return self.buf.tobytes()
 
@@ -46,23 +49,23 @@ class SharedNumpyArray:
 
     def __getstate__(self):
         state = self.__dict__.copy()
-        del state["shm"]
-        state.pop("buf", None)
-        state["created"] = False
-        state["shm_name"] = self.shm.name
+        del state['shm']
+        state.pop('buf', None)
+        state['created'] = False
+        state['shm_name'] = self.shm.name
         return state
 
     def __setstate__(self, state):
         state = state.copy()
-        shm_name = state.pop("shm_name")
+        shm_name = state.pop('shm_name')
         self.__dict__.update(state)
         self.shm = multiprocessing.shared_memory.SharedMemory(shm_name)
         self.buf = np.ndarray(self.shape, dtype=self.dtype, buffer=self.shm.buf)
 
     def __del__(self):
-        if hasattr(self, "shm"):
+        if hasattr(self, 'shm'):
             self.shm.close()
-            if getattr(self, "created", None) is True:
+            if getattr(self, 'created', None) is True:
                 self.shm.unlink()
 
 
@@ -125,7 +128,7 @@ def _hs_flatten(slide, study):
         flattened = [
             [
                 {
-                    **_hs_study_meta(slide),
+                    **_hs_study_meta(study),
                     **_hs_slide_meta(slide, study),
                     **_hs_tile_meta(tile, chunk, study),
                 }
@@ -155,221 +158,170 @@ def _hs_flatten(slide, study):
     return flattened
 
 
-def _largeimage_kwargs(slide, study):
-    """Transform histomics_stream tile definitions to read kwargs for
-    use with large_image. Return read keyword arguments and corresponding
-    study values."""
+class TiffPrefetch(object):
+    """An eager tile iterator for large_image_tiff formats.
 
-    def tile_dict(tile, slide, study):
-        return dict(
-            scale={"magnification": slide["target_magnification"]},
-            format="numpy",
-            region=dict(
-                left=tile["tile_left"],
-                top=tile["tile_top"],
-                width=study["tile_width"],
-                height=study["tile_height"],
-                units="mag_pixels",
-            ),
-            tile_size=dict(width=study["tile_width"], height=study["tile_height"]),
-        )
-
-    flattened = _hs_flatten(slide, study)
-    if "chunks" in slide.keys():
-        read_kwargs = [[tile_dict(t, slide, study) for t in c] for c in flattened]
-    else:
-        read_kwargs = [tile_dict(t, slide, study) for t in flattened]
-
-    return read_kwargs, flattened
-
-
-class LargeimagePrefetch(object):
-    """A prefetching tile iterator for large_image sources.
-
-    This iterator generates tile batches via multiprocessing. If chunk size
-    is specified in input study, each process will read one chunk. Otherwise
-    each process reads one tile.
+    This iterator generates tile batches via multiprocessing. Each process
+    reads a group of tiles to minimize reads.
 
     Parameters
     ----------
     study : dict
         A histomics stream study.
     icc : bool
-        Whether to apply ICC correction. Default value is True.
+        Whether to attempt ICC correction.
     batch : int
-        Size of generated batches. Partial batches are not padded. Default
-        value is 64.
+        The batch size. A partial batch at the end will not be padded.
     prefetch : int
-        The number of prefetched batches to maintain.
+        The target number of prefetched batches.
     workers : int
         The number of multiprocessing workers.
     """
 
-    def __init__(self, study, icc=False, batch=64, prefetch=4, workers=32):
+    def __init__(self, study, icc=False, batch=64, prefetch=16, workers=16):
         if len(study["slides"]) > 1:
             raise ValueError("Multi-slide studies not supported.")
-        slide = list(study["slides"].values())[0]
         self.pool = ProcessPoolExecutor(max_workers=workers)
+        slide = list(study["slides"].values())[0]
         self.source = large_image_source_tiff.open(
-            slide["filename"], style={"icc": False} if not icc else None
+            slide["filename"],
+            style={"icc": False} if not icc else None
         )
-        self.read_kwargs, self.meta = _largeimage_kwargs(slide, study)
-        self.group = "chunks" in slide.keys()
+        self.read_kwargs = _hs_flatten(slide, study)
+        self._initialize(batch, prefetch)
+        
+    def _initialize(self, batch, prefetch):
         self.prefetch = prefetch
         self.batch = batch
-        self.pos = 0
-        if self.group:
-            self._initialize_group(batch, prefetch)
-        else:
-            self._initialize_single(batch, prefetch)
-
-    def _initialize_single(self, batch, prefetch):
-        self.queue = Queue(prefetch)
-        self.pos = 0  # tracks number of read tiles
-        self._fill_single()
-
-    def _initialize_group(self, batch, prefetch):
-        self.queue = Queue()  # hold futures defining read operations
-        self.pending = 0  # number of pending tiles in futures
-        self.remainder = ([], [])  # save overflow for following batch
-        self._fill_group()
+        self.queue = deque([]) # hold futures defining read operations
+        self.overflow = 0 # count of tile overrun for latest batch
+        self.pos = 0  # position in read_kwargs
+        self._fill()
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.group:
-            return self._next_group()
-        else:
-            return self._next_single()
-
-    def _next_group(self):
-        if (
-            (self.pos == len(self.read_kwargs))
-            and (self.pending == 0)
-            and (len(self.remainder[0]) == 0)
-        ):
+        if (self.pos == len(self.read_kwargs)) and not len(self.queue):
             raise StopIteration
 
-        # add partial batch remainder samples
-        tiles, meta = self.remainder
-        self.remainder = ([], [])
+        # wait on the futures linked to the next batch
+        try:
+            futures, tiles, read_kwargs = self.queue.pop()
+            wait(futures, timeout=None, return_when=ALL_COMPLETED)
+            self._fill()
+        except:
+            self.pool.shutdown(wait=False, cancel_futures=True)
+            raise
 
-        # fill to full batch or exhaustion
-        while (len(tiles) < self.batch) and (self.pending > 0):
-            future, m = self.queue.get()
-            wait([future], timeout=None, return_when=FIRST_COMPLETED)
-            try:
-                reads = future.result()
-            except Exception as error:
-                self.pool.shutdown(wait=False, cancel_futures=True)
-                raise
-            if len(reads) + len(tiles) > self.batch:
-                self.remainder = (
-                    reads[self.batch - len(tiles) :],
-                    m[self.batch - len(tiles) :],
-                )
-            meta = meta + m[0 : min(len(reads), self.batch - len(tiles))]
-            tiles = tiles + reads[0 : min(len(reads), self.batch - len(tiles))]
-            self.pending = self.pending - len(reads)
-        self._fill_group()
-        return np.stack(tiles), meta
+        # last batch may only have partial size
+        if self.pos == len(self.read_kwargs) and not len(self.queue):
+            tiles.shape = [len(read_kwargs), *tiles.shape[1:]]
+
+        return tiles, read_kwargs
 
     @staticmethod
-    def read_group(source, sharr, read_kwargs):
+    def read(source, read_kwargs, sharrs, offset, batch):
         # read followed by crops
-        xt = [k["region"]["left"] for k in read_kwargs]
-        yt = [k["region"]["top"] for k in read_kwargs]
-        wt = [k["region"]["width"] for k in read_kwargs]
-        ht = [k["region"]["height"] for k in read_kwargs]
+        xt = [k["tile_left"] for k in read_kwargs]
+        yt = [k["tile_top"] for k in read_kwargs]
+        wt = [k["tile_width"] for k in read_kwargs]
+        ht = [k["tile_height"] for k in read_kwargs]
         xr = min(xt)
         yr = min(yt)
-        wr = max([x + w for (x, w) in zip(xt, wt)]) - xr
-        hr = max([y + h for (y, h) in zip(yt, ht)]) - yr
-        read_dict = deepcopy(read_kwargs[0])
-        read_dict["region"]["left"] = xr
-        read_dict["region"]["top"] = yr
-        read_dict["region"]["width"] = wr
-        read_dict["region"]["height"] = hr
-        read_dict["tile_size"] = {"width": wr, "height": hr}
+        wr = max([x+w for (x,w) in zip(xt,wt)])-xr
+        hr = max([y+h for (y,h) in zip(yt,ht)])-yr
+        kwargs = dict(
+            scale={"magnification": read_kwargs[0]["target_magnification"]},
+            format="numpy",
+            region=dict(left=xr, top=yr, width=wr, height=hr, units="mag_pixels"),
+            tile_size=dict(width=wr, height=hr),
+        )
         chunk, _ = source.getRegion(**read_dict)
-        for s, x, y, w, h in zip(sharr, xt, yt, wt, ht):
-            s.copy(chunk[y - yr : y - yr + h, x - xr : x - xr + w, :])
-        return sharr
-
-    def _submit_group(self):
+        tiles = [
+            chunk[y-yr:y-yr+h, x-xr:x-xr+w, :]
+            for (x,y,w,h) in zip(xt,yt,wt,ht)
+        ]
+        for i, tile in enumerate(tiles):
+            sharr_index, slice_index = divmod(offset + i, batch)
+            sharrs[sharr_index].insert(tile, slice_index)
+    
+    def _submitfn(self, read_kwargs, sharrs, offset):
         return self.pool.submit(
-            self.read_group,
+            self.read,
             self.source,
-            [
-                SharedNumpyArray(
-                    (r["region"]["height"], r["region"]["width"], 3), np.uint8
-                )
-                for r in self.read_kwargs[self.pos]
-            ],
-            self.read_kwargs[self.pos],
+            read_kwargs,
+            sharrs,
+            offset,
+            self.batch,
         )
 
-    def _fill_group(self):
+    def _fill(self):
         try:
-            while len(
-                self.remainder[0]
-            ) + self.pending < self.batch * self.prefetch and self.pos < len(
-                self.read_kwargs
-            ):
-                self.queue.put((self._submit_group(), self.meta[self.pos]))
-                self.pending = self.pending + len(self.read_kwargs[self.pos])
-                self.pos = self.pos + 1
-        except Exception as error:
-            self.pool.shutdown(wait=False, cancel_futures=True)
-            raise
+            while len(self.queue) < self.prefetch and self.pos < len(self.read_kwargs):
+                """if the last read from the prior batch spanned batch boundaries then
+                the leftmost element in self.queue contains the futures, shared array,
+                and read_kwargs to start the current batch"""
+                if self.overflow:
+                    futures, tiles, batch_kwargs = self.queue.popleft()
+                else:
+                    """last read aligned with batch boundary, create new shared array,
+                    and read_kwargs, futures containers"""
+                    tiles = SharedNumpyArray(
+                        [self.batch, 
+                         self.read_kwargs[self.pos][0]["tile_height"], 
+                         self.read_kwargs[self.pos][0]["tile_width"],
+                         3],
+                         np.uint8)
+                    futures = []
+                    batch_kwargs = []
 
-    def _next_single(self):
-        if self.pos >= len(self.read_kwargs):
-            raise StopIteration
-        futures = self.queue.get()
-        wait(futures, timeout=None, return_when=ALL_COMPLETED)
-        self.pos += len(futures)
-        try:
-            batch = [(f.result(), m) for (f, m) in futures.items()]
-            tiles = [b[0] for b in batch]
-            meta = [b[1] for b in batch]
-        except Exception as error:
-            self.pool.shutdown(wait=False, cancel_futures=True)
-            raise
-        self._fill_single()
-        return np.stack(tiles), meta
+                """submit enough jobs to fill at least one batch - a single job may 
+                fill multiple batches - or multiple jobs may be needed to fill one 
+                batch"""
+                offset = self.overflow
+                batches = 1
+                tiles = [tiles]
+                while len(batch_kwargs) < self.batch and \
+                self.pos < len(self.read_kwargs):
+                    # number of batches spanned by this read
+                    reads = self.read_kwargs[self.pos]
+                    batches = math.ceil((len(reads) + offset) / self.batch)
 
-    @staticmethod
-    def read_single(source, sharr, **kwargs):
-        tile, _ = source.getRegion(**kwargs)
-        sharr.copy(tile)
-        return sharr
+                    # create additional arrays if this read spans multiple batches
+                    tiles = [*tiles, 
+                            *[SharedNumpyArray(
+                                [self.batch, 
+                                 reads[0]["tile_height"],
+                                 reads[0]["tile_width"],
+                                 3],
+                                np.uint8)
+                                for _ in range(batches-1)]
+                            ]
 
-    def _submit_single(self, *args, **kwargs):
-        return self.pool.submit(
-            self.read_single,
-            self.source,
-            SharedNumpyArray(
-                (kwargs["region"]["height"], kwargs["region"]["width"], 3), np.uint8
-            ),
-            **kwargs,
-        )
+                    """submit job - read into first array in `tiles` at slice `offset`.
+                    overflow to subsequent arrays if this read spans multiple batches.
+                    if multiple reads are required to fill this batch then increment
+                    the offset and submit another job on the next iteration"""
+                    futures.append(self._submitfn(reads, tiles, offset))
+                    offset = offset + len(reads)
+                    batch_kwargs = batch_kwargs + reads
+                    self.pos = self.pos + 1
+                self.overflow = len(batch_kwargs) % self.batch
 
-    def _fill_single(self):
-        try:
-            start = self.pos + self.batch * self.queue.qsize()
-            while not self.queue.full() and start < len(self.read_kwargs):
-                futures = {}
-                for k, m in zip(
-                    self.read_kwargs[
-                        start : min(start + self.batch, len(self.read_kwargs))
-                    ],
-                    self.meta[start : min(start + self.batch, len(self.meta))],
-                ):
-                    futures[self._submit_single(**k)] = m
-                self.queue.put(futures)
-                start += self.batch  # Move to the next batch
+                """ if last read spans multipe batches, link that read's future
+                to the other batches - also divide kwargs according to batch boundaries
+                """
+                futures = [futures] + (batches-1) * [[futures[-1]]]
+                batch_kwargs = [
+                    batch_kwargs[i:i+self.batch] 
+                    for i in range(0, len(batch_kwargs), self.batch)
+                ]
+
+                # enqueue batches
+                for f, t, b in zip(futures, tiles, batch_kwargs):
+                    self.queue.appendleft((f, t, b))
+
         except Exception as error:
             self.pool.shutdown(wait=False, cancel_futures=True)
             raise
