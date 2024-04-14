020fa5bc521310c9d849eda2a50928a08a556f3b
Lee Cooper <cooperle@gmail.com>
2024-04-14 01:04:24 -0500

    simplify input casting and deconvolution preprocessing

--- simple_triton/encoders.py
+++ simple_triton/encoders.py
@@ -61,63 +61,6 @@ def reshape_savedmodel(
     )
 
 
-def _deconv_model(extractor):
-    """Create a tensorflow deconvolution model for prepend to a tensorflow
-    feautre extraction model.
-
-    This model contains a non-trainable layer that performs color deconvolution
-    using a stain matrix matched to the input, and reconvolves the stain
-    concentrations with an ideal stain matrix. It contains three inputs: 1.
-    The batched images 2. The stain matrix for the input data and 3. The ideal
-    stain matrix.
-
-    Parameters
-    ----------
-    extractor : tf.keras.Model
-        A tensorflow feature extractor model.
-
-    Returns
-    -------
-    deconv : tf.keras.Model
-        A model containing a non-trainable normalization layer, and
-        inputs for batched images, and source and target stain matrices.
-
-    See also
-    --------
-    deconvolution_based_normalization from histomicstk.preprocessing.normalization.
-    """
-
-    # raise error if feature extractor has more than one input
-    if len(extractor.inputs) > 1:
-        raise ValueError(
-            (
-                "Feature extractor must have one input, "
-                f"has {len(extractor.inputs)} instead."
-            )
-        )
-
-    # get extractor input layer parameters and shape
-    extractor_config = extractor.get_config()
-    input_kwargs = {
-        k: extractor_config["layers"][0]["config"][k]
-        for k in ["dtype", "sparse", "ragged"]
-    }
-    shape = list(extractor.inputs[0].shape)
-    if shape[0] is None:
-        shape = shape[1:]
-
-    # create input layers
-    input_0 = tf.keras.layers.Input(shape=shape, **input_kwargs, name="input_0")
-    input_1 = tf.keras.layers.Input(shape=[3, 3], name="input_1")
-    input_2 = tf.keras.layers.Input(shape=[3, 3], name="input_2")
-
-    # create deconv layer and model
-    deconv_layer = DeconvNorm()([input_0, input_1, input_2])
-    deconv = tf.keras.Model([input_0, input_1, input_2], deconv_layer)
-
-    return deconv
-
-
 def _nested_replace(inbound, replacement):
     if isinstance(inbound, list):
         inbound = [_nested_replace(l, replacement) for l in inbound]
@@ -241,7 +184,14 @@ def _tf_rename_outputs(model, output_index=0):
 
 
 def tf_encoder(
-    repository, model, name, input_shape=(224, 224, 3), pooling="avg", normalize=False
+    repository,
+    model,
+    name,
+    input_shape=(224, 224, 3),
+    dtype=tf.uint8,
+    pooling="avg",
+    normalize=False,
+    version=1,
 ):
     """Creates a tensorflow encoder model in savedmodel format.
 
@@ -261,12 +211,18 @@ def tf_encoder(
     input_shape : tuple(int, int, int)
         The height, width, and channels of tiles used for feature extraction at the
         target magnification. Default value is (224, 224, 3).
+    dtype : tf.dtypes.DType
+        Input type. Default value is `tf.uint8` to minimize host-device transfer.
+        Will be cast to float following input.
     pooling : str {"avg", "max"}
         The pooling mode for the terminal layer of the feature extractor network.
     normalize : bool
         Include a deconvolution-based color normalization layer at the input. Requires
         providing stain matrices for both the input and ideal color profile. Default
         value is False.
+    version : int
+        The model version. Triton expects tensorflow models to be saved under a model
+        version folder e.g. /repository/{name}.tensorflow/{version}/model.savedmodel.
 
     Returns
     -------
@@ -366,22 +322,39 @@ def tf_encoder(
     else:
         raise ValueError("model not recognized.")
 
-    # prepend normalization layer
+    # remove encoder input layer and extract config
+    input_layer = model.layers.pop(0)
+    input_kwargs = input_layer.get_config()
+
+    # add input layer and optional devonvolution layer
+    # use scope to address LayerScale layer if ConvNext model selected
+    input_0 = tf.keras.layers.Input(
+        shape=input_shape,
+        dtype=dtype,
+        name="input_0",
+        sparse=input_kwargs["sparse"],
+        ragged=input_kwargs["ragged"],
+    )
+    float32_input = (
+        tf.keras.layers.Lambda(tf.keras.backend.cast_to_floatx)(input_0)
+        if dtype != tf.float32
+        else input_0
+    )
     with keras.utils.custom_object_scope(custom_objects):
         if normalize:
-            deconv = _deconv_model(model)
-            deconv = _tf_rename_inputs(deconv)
-            model = _tf_rename_inputs(model, 3)
-            model = tf.keras.Model(deconv.inputs, model(deconv.outputs))
-            model = _tf_rename_outputs(model)
+            input_1 = tf.keras.layers.Input(shape=[3, 3], name="input_1")
+            input_2 = tf.keras.layers.Input(shape=[3, 3], name="input_2")
+            deconv = DeconvNorm()([float32_input, input_1, input_2])
+            model = tf.keras.Model([input_0, input_1, input_2], model(deconv))
         else:
-            model = _tf_rename_outputs(_tf_rename_inputs(model))
+            model = tf.keras.Model(input_0, model(float32_input))
+    model = _tf_rename_outputs(_tf_rename_inputs(model))
 
     # get dimensionality of extracted features
     D = model.output_shape[-1]
 
     # create the output folder
-    path = os.path.join(repository, name, "1", "model.savedmodel")
+    path = os.path.join(repository, name, str(version), "model.savedmodel")
     os.makedirs(path)
 
     # save model
