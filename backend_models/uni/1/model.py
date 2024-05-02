caa43848c5e297cf9c46c5acc7fee04168d2c7a8
marinayad <marina.ayad2@gmail.com>
2024-05-02 11:38:21 -0500

    Add UNI model wrapper and update phikon to run on gpu

--- backend_models/uni/1/model.py
+++ backend_models/uni/1/model.py
@@ -1,18 +1,21 @@
 import triton_python_backend_utils as pb_utils
-import torch
 import json
-from transformers import AutoImageProcessor, ViTModel
 import numpy as np
 import tritonclient.utils as triton_utils
+import timm
+import torch
+from huggingface_hub import login
+from PIL import Image
+import os
 
 
 class TritonPythonModel:
 
     def initialize(self, args):
         """
-        This function initializes pre-trained phikon model from hugging face.
+        This function initializes pre-trained UNI model from hugging face.
         The initialization is depending on the configuration from 'config.pbtxt'
-        "https://huggingface.co/owkin/phikon"
+        https://huggingface.co/MahmoodLab/UNI
         """
         #Load Model configuration
         self.model_config = model_config = json.loads(args["model_config"])
@@ -25,11 +28,17 @@ class TritonPythonModel:
         self.output0_dtype = pb_utils.triton_string_to_numpy(
             output0_config["data_type"]
         )
+        login(os.getenv("TOKEN"))  # login with your User Access Token, found at https://huggingface.co/settings/tokens
 
-        #Initialize the pretrained Phikon Model
-        self.model = ViTModel.from_pretrained("owkin/phikon", add_pooling_layer=False)
+        #Initialize the pretrained UNI Model
+        self.model = timm.create_model("hf-hub:MahmoodLab/uni", pretrained=True, init_values=1e-5, dynamic_img_size=True)
         self.model = self.model.to(torch.device(f"cuda:{self.gpu_id}"))
-        self.image_processor = AutoImageProcessor.from_pretrained("owkin/phikon")
+        
+        #These values are obtained from UNI hugging face example
+        self.mean = np.array([0.485, 0.456, 0.406])
+        self.std = np.array([0.229, 0.224, 0.225])
+        self.model.eval()
+ 
         
     def execute(self, requests):
         """
@@ -38,18 +47,21 @@ class TritonPythonModel:
         """
         
         responses = []
-
         for request in requests:
 
             try:
                 in_0 =pb_utils.get_input_tensor_by_name(request, "input_0")
                 input_np = in_0.as_numpy()
-                inputs = self.image_processor(input_np, return_tensors="pt").to(torch.device(f"cuda:{self.gpu_id}"))
-
-                with torch.no_grad():
-                    outputs = self.model(**inputs)
-                    features = outputs.last_hidden_state[:,0,:] # shape (1, 768)
-                    features = features.detach().cpu().numpy()
+                input_np = np.transpose(input_np, (0,3,1,2))
+                input_tensor = torch.tensor(input_np)
+                input_norm =(input_tensor / 255.0 - self.mean.reshape((1,3,1,1)))/(self.std.reshape((1,3,1,1)))
+                input_norm = input_norm.float().to(torch.device(f"cuda:{self.gpu_id}"))
+            
+                with torch.inference_mode():
+                    feature_emb =  self.model(input_norm)
+                    features = feature_emb.detach().cpu().numpy()
+                    print(features)
+                    
 
                 out_tensor_features = pb_utils.Tensor("output_0", features.astype(np.float32))
                 inference_response = pb_utils.InferenceResponse(
