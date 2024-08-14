52f62d1574479718de48bae1240f9c69cbce1cbd
Anders Sildnes <andsild@posteo.net>
2024-08-14 17:27:03 -0500

    Use np.dtype('...') instead for more accurate isinstance guesswork
    Also necessary in newer versions of python

--- simple_triton/feature_extraction.py
+++ simple_triton/feature_extraction.py
@@ -513,6 +513,8 @@ def main():
                 print(f"Inference error {file}: {exc}")
                 continue
 
+            precision = np.dtype('float32') if args.float else np.dtype('float16')
+
             # tile iterator
             iterator = TiffPrefetch(
                 hs_study, dtype, args.icc, args.batch, args.prefetch, args.workers
