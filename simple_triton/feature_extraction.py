3170c513cecf258641a639f5a78bf9c965a4701a
Lee Cooper <cooperle@gmail.com>
2024-08-16 13:10:55 -0500

    use numpy dtypes to define precision

--- simple_triton/feature_extraction.py
+++ simple_triton/feature_extraction.py
@@ -10,7 +10,6 @@ from simple_triton.model import TritonModel
 from simple_triton.tile_iterators import TiffPrefetch
 from time import sleep, time
 from tqdm import tqdm
-import tensorflow as tf
 
 
 def study(
@@ -543,7 +542,7 @@ def main():
             features = np.concatenate(features[0], axis=0)
 
             # write to tfrecord
-            precision = tf.float32 if args.float else tf.float16
+            precision = np.float32 if args.float else np.float16
             write_record(
                 tfr_name(
                     args.output,
