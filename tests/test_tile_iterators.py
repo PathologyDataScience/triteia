d17c2dd72bfb77e0d92d80da36e05726797379e2
cooperlab <cooperle@gmail.com>
2024-08-21 13:03:22 -0500

    passing tile_iterator tests

--- tests/test_tile_iterators.py
+++ tests/test_tile_iterators.py
@@ -1,65 +1,108 @@
+from .data import data
+import hashlib
+import json
 import numpy as np
 import os
+import pickle
 import pooch
 import pytest
 from simple_triton.feature_extraction import study
 from simple_triton.tile_iterators import TiffPrefetch
 
 
-# download whole slide image and corresponding mask
-wsi_path = pooch.retrieve(
-    fname="TCGA-AN-A0G0-01Z-00-DX1.svs",
-    url="https://drive.usercontent.google.com/download?id=19agE_0cWY582szhOVxp9h3kozRfB4CvV&export=download&confirm=t",
-    known_hash="d046f952759ff6987374786768fc588740eef1e54e4e295a684f3bd356c8528f",
-    path=str(pooch.os_cache("pooch")) + os.sep + "wsi",
-)
-mask_path = pooch.retrieve(
-    fname="TCGA-AN-A0G0-01Z-00-DX1.mask.png",
-    url="https://drive.usercontent.google.com/download?id=17GOOHbL8Bo3933rdIui82akr7stbRfta&export=download&confirm=t",
-    known_hash="bb657ead9fd3b8284db6ecc1ca8a1efa57a0e9fd73d2ea63ce6053fbd3d65171",
-    path=str(pooch.os_cache("pooch")) + os.sep + "wsi",
-)
+HASH_KEYS = {"tile_height", "tile_width", "tile_top", "tile_left", "target_magnification"}
 
 
-# slide parameters
-tile = 224
-batch = 64
-magnification = 20.0
-chunk = 896
-mask_threshold = 0.5
-prefetch = 4
-workers = 16
-icc = True
-nchw = False
+def hash_dict(d):
+    """hash a possibly nested dictionary"""
+    def immutify(d):
+        d_new = {}
+        for k, v in d.items():
+            if isinstance(v, np.ndarray):
+                d_new[k] = tuple(v.tolist())
+            elif isinstance(v, list):
+                d_new[k] = tuple(v)
+            elif isinstance(v, dict):
+                d_new[k] = immutify(v)
+            else:
+                # convert numpy types to native
+                if hasattr(v, "dtype"):
+                    d_new[k] = v.item()
+                else:
+                    d_new[k] = v
+        return dict(sorted(d_new.items(), key=lambda item: item[0]))
+    d_hashable = immutify(d)
+    s_hashable = json.dumps(d_hashable).encode("utf-8")
+    m = hashlib.sha256(s_hashable).hexdigest()
+    return m
 
 
-# create a histomics-stream study from a wsi/mask pair
-hs_study = study(
-    (wsi_path, mask_path),
-    t=(tile, tile),
-    chunk=(chunk, chunk),
-    objective=magnification,
-    mask_threshold=mask_threshold,
-)
+def compare_dict(x, y):
+    """ensure equality of two dictionaries containing hashes"""
+    assert set(x.keys()) == set(y.keys())
+    assert all([x[mhash] == y[mhash] for mhash in x.keys()])
 
 
-def test_nonzero():
+def meta_tile_dict(batched):
+    """build a dictionary linking tile metadata hashes with tile pixel hashes"""
+    linked = {}
+    for (batch, metadata) in batched:
+        for (tile, m) in zip(batch, metadata):
+            linked[hash_dict(m)] = hashlib.sha256(tile.tobytes()).hexdigest()
+    return linked
+
+
+def pkl_tiles(path, kwargs):
+    """pickle a hashed version of tile iterator outputs"""
+    iterator = TiffPrefetch(**kwargs)
+    keys = {"tile_height", "tile_width", "tile_top", "tile_left", "target_magnification"}
+    batches = [(i, [{k: s[k] for k in keys} for s in m]) for (i, m) in iterator]
+    hashed = meta_tile_dict(batches)
+    with open(path, "wb") as f:
+        pickle.dump({"kwargs": kwargs, "hashed": hashed}, f, pickle.HIGHEST_PROTOCOL)
+
+
+def unpkl_tiles(path):
+    """load hashed version of tile iterator outputs"""
+    with open(path, "rb") as f:
+        contents = pickle.load(f)
+    return contents["kwargs"], contents["hashed"]
+
+
+def test_contents_icc_batched(data):
+    """Compare tile contents to gold-standard with ICC correction"""
+    kwargs, hashed = unpkl_tiles(data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_icc.pkl"))
+    iterator = TiffPrefetch(**kwargs)
+    batches = [(i, [{k: s[k] for k in HASH_KEYS} for s in m]) for (i, m) in iterator]
+    compare_dict(meta_tile_dict(batches), hashed)
+
+
+def test_contents_noicc_batched(data):
+    """Compare tile contents to gold-standard without ICC correction"""
+    kwargs, hashed = unpkl_tiles(data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_no_icc.pkl"))
+    iterator = TiffPrefetch(**kwargs)
+    batches = [(i, [{k: s[k] for k in HASH_KEYS} for s in m]) for (i, m) in iterator]
+    compare_dict(meta_tile_dict(batches), hashed)
+
+
+def test_nonzero(data):
     """Ensure that tiles are nonzero"""
+    kwargs, _ = unpkl_tiles(data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_icc.pkl"))
+    iterator = TiffPrefetch(**kwargs)
+    batched = [b for (b, m) in iterator]
+    for b in batched:
+        assert not np.array_equal(b.view(), np.zeros(b.shape, b.dtype))
 
-    # create tile iterators and pull tiles
-    iterator = TiffPrefetch(hs_study, np.uint8, nchw, icc, batch, prefetch, workers)
-    tiles = [t for (t, m) in iterator]
-    for t in tiles:
-        assert not np.array_equal(t.view(), np.zeros(t.shape, t.dtype))
 
-
-def test_nchw():
+def test_nchw(data):
     """Ensure that transposed nchw/nhwc batches have the same contents"""
-
-    # create tile iterators and pull tiles
-    iterator = TiffPrefetch(hs_study, np.uint8, False, icc, batch, prefetch, workers)
+    kwargs, _ = unpkl_tiles(data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_icc.pkl"))
+    kwargs["nchw"] = False
+    iterator = TiffPrefetch(**kwargs)
     nhwc = [t for (t, m) in iterator]
-    iterator = TiffPrefetch(hs_study, np.uint8, True, icc, batch, prefetch, workers)
+    kwargs["nchw"] = True
+    iterator = TiffPrefetch(**kwargs)
     nchw = [t for (t, m) in iterator]
     for f, t in zip(nhwc, nchw):
         assert np.array_equal(f.view(), np.transpose(t.view(), [0, 2, 3, 1]))
+
