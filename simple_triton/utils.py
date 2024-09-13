7e311914b24da48efd80528e4271ff09b9dde7dc
Anders Sildnes <andsild@posteo.net>
2024-09-13 16:37:20 -0500

    Change analyze to return dataframe

--- simple_triton/utils.py
+++ simple_triton/utils.py
@@ -29,7 +29,7 @@ def create_client(url="localhost:8001", verbose=False):
         raise
 
 def analyze(times, floatfmt=".2f"):
-    """Print a summary table of time spent during inference.
+    """Print and return a summary table of time spent during inference as a pandas DataFrame.
 
     This table summarizes the total and fractional time spent loading data,
     submitting inference requests, completing requests, and retrieving
@@ -42,39 +42,34 @@ def analyze(times, floatfmt=".2f"):
         read_stop, submitted, retrieved, completed times for each inference.
     floatfmt : str
         A format string for float printing.
+    
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame summarizing the timing statistics.
     """
 
-    # calculate times
+    # Calculate times
     total = np.subtract(times["retrieved"], times["read_start"])
     read = np.subtract(times["read_stop"], times["read_start"])
     submission = np.subtract(times["submitted"], times["read_stop"])
     completion = np.subtract(times["completed"], times["submitted"])
     retrieval = np.subtract(times["retrieved"], times["completed"])
 
-    # convert to percentages
+    # Convert to percentages
     read_frac = [100.0 * r / t for (t, r) in zip(total, read)]
     completion_frac = [100.0 * c / t for (t, c) in zip(total, completion)]
     retrieval_frac = [100.0 * r / t for (t, r) in zip(total, retrieval)]
     submission_frac = [100.0 * s / t for (t, s) in zip(total, submission)]
 
     def stats(x):
-        return [np.median(x), np.std(x), min(x), max(x)]
+        return [np.median(x), np.std(x), np.mean(x), min(x), max(x)]
 
-    # form table
-    table = [
-        ["total (sec)", *stats(total)],
-        ["read (sec)", *stats(read)],
-        ["submission (sec)", *stats(submission)],
-        ["completion (sec)", *stats(completion)],
-        ["retrieval (sec)", *stats(retrieval)],
-        SEPARATING_LINE,
-        ["read (% total)", *stats(read_frac)],
-        ["submission (% total)", *stats(submission_frac)],
-        ["completion (% total)", *stats(completion_frac)],
-        ["retrieval (% total)", *stats(retrieval_frac)],
-    ]
+    data = pd.DataFrame(data=[stats(total), stats(read), stats(submission), stats(completion), stats(retrieval), stats(read_frac), stats(submission_frac), stats(completion_frac), stats(retrieval_frac)],
+        index=["total (sec)", "read (sec)", "submission (sec)", "completion (sec)", "retrieval (sec)", "read (% total)", "submission (% total)", "completion (% total)", "retrieval (% total)"],
+        columns=['median', 'stdv', 'avg', 'min', 'max'],
+        )
 
-    # display results
-    print(
-        tabulate(table, headers=["", "median", "stdv", "min", "max"], floatfmt=floatfmt)
-    )
+    df = pd.DataFrame(data)
+
+    return df
