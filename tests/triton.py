dade7b0e0bdf274dfc92de991c0a6e12ee1d3839
cooperlab <cooperle@gmail.com>
2024-09-24 16:22:14 -0500

    update docker manipulation functions

--- tests/triton.py
+++ tests/triton.py
@@ -1,3 +1,4 @@
+from functools import partial
 import pytest
 import subprocess
 from time import sleep, time
@@ -10,22 +11,31 @@ not present on the system. The `triton` fixture receives the model repository fr
 data fixture which creates a temporary directory.
 """
 
-LAUNCH_TIMEOUT = 10.0  # short timeout for triton launch if image available
-TRITON_IMAGE_NAME = "nvcr.io/nvidia/tritonserver"
-TRITON_IMAGE_ID = "f6f448fbf332"
+TIMEOUT = 10.0  # short timeout for container run, stop operations (not build)
+TRITON_IMAGE_NAME = "model-tritonserver"
 
-"""Host tritonserver run command parameterized by `repository`"""
-cmd = (
+"""commands to stop, build, and run dockers parameterized by `param`"""
+stop_cmd = (
+    "docker stop $(docker ps -a -q --filter ancestor={param} --format=\"{{.ID}}\")"
+)
+exited_cmd = (
+    "docker ps -f status=running -f ancestor={param}"
+)
+build_cmd = (
+    "docker build -t model-tritonserver -f {param}/models.Dockerfile ."
+)
+run_cmd = (
     "docker run --gpus=all -d --rm -p8000:8000 -p8001:8001 -p8002:8002 -p8003:8003 "
     "--shm-size=1g --ulimit memlock=-1 --ipc=host "
-    "-v {repository}:/models "
+    "-v {param}:/models "
     "nvcr.io/nvidia/tritonserver:23.03-py3 "
     "tritonserver --model-repository=/models "
     "--model-control-mode=explicit --exit-on-error=false "
 )
+cmd = partial(subprocess.run, shell=True, capture_output=True, text=True)
 
 
-"""Launch the triton container - block until responsive and stop container on exit"""
+"""Launch the triton container - block until responsive"""
 
 
 def triton_launch(model_repository):
@@ -37,35 +47,32 @@ def triton_launch(model_repository):
             ),
             returncode=2,
         )
-    container_id = subprocess.run(
-        cmd.format(repository=model_repository),
-        shell=True,
-        capture_output=True,
-        text=True,
-    )
+    container_id = cmd(run_cmd.format(param=model_repository))
     start = time()
-    while not triton_ready() and time() - start < LAUNCH_TIMEOUT:
+    while not triton_ready() and time() - start < TIMEOUT:
+        sleep(1.0)
+
+
+"""Stop the triton containers - block until stopped"""
+
+
+def triton_stop(name):
+    response = cmd(stop_cmd.format(param=name))
+    start = time()
+    response = cmd(exited_cmd.format(param=name))
+    while len(response.stdout.split("\n")) > 1 and time() - start < TIMEOUT:
+        response = cmd(exited_cmd.format(param=name))
         sleep(1.0)
 
 
 def triton_ready():
-    response = subprocess.run(
-        'curl -v --silent localhost:8000/v2/health/ready 2>&1 | grep -m 1 "<"',
-        shell=True,
-        capture_output=True,
-        text=True,
-    )
+    response = cmd('curl -v --silent localhost:8000/v2/health/ready 2>&1 | grep -m 1 "<"')
     return response.stdout.strip() == "< HTTP/1.1 200 OK"
 
 
 def triton_image_available():
-    response = subprocess.run(
-        f"docker images {TRITON_IMAGE_NAME}",
-        shell=True,
-        capture_output=True,
-        text=True,
-    )
-    return TRITON_IMAGE_ID in result.stdout.split("\n")[1]
+    response = cmd(f"docker images {TRITON_IMAGE_NAME}")
+    return TRITON_IMAGE_ID in response.stdout.split("\n")[1]
 
 
 @pytest.fixture(scope="session")
