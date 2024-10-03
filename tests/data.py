d5f3f2d29cd7126059f155608a66f7f0b7e28832
cooperlab <cooperle@gmail.com>
2024-10-03 12:09:54 -0500

    refactor tests

--- tests/data.py
+++ tests/data.py
@@ -1,4 +1,8 @@
+import hashlib
+import json
+import numpy as np
 import os
+import pickle
 import pooch
 import pytest
 import tempfile
@@ -7,6 +11,15 @@ import tempfile
 """Data for testing and examples are hosted on Google Drive. Pooch is used to download
 this data by parsing registry.csv and prefetching these data once per testing session.
 Downloads are deleted when tests are completed."""
+
+
+HASH_KEYS = {
+    "tile_height",
+    "tile_width",
+    "tile_top",
+    "tile_left",
+    "target_magnification",
+}
 
 
 def registry():
@@ -52,3 +65,120 @@ def data(files=None):
         )
         data.prefetch(files)
         yield data
+
+
+def replace_study_path(study, path):
+    """replace wsi path within study object"""
+    for image in study["slides"].keys():
+        study["slides"][image]["filename"] = path
+    return study
+
+
+def tile_pkl(pkl_path, wsi_path):
+    """load a tile pkl containing the study kwargs, metadata, and hashed pixels"""
+    with open(pkl_path, "rb") as f:
+        contents = pickle.load(f)
+    contents["kwargs"]["study"] = replace_study_path(wsi_path)
+    return contents["kwargs"], contents["hashed"]
+
+
+@pytest.fixture
+def tiles_icc(data):
+    """returns a dict linking hashed tile metadata and hashed icc tile pixels values"""
+    _, hashed = tile_pkl(
+        data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_icc.pkl"),
+        data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs"),
+    )
+    return hashed
+
+
+@pytest.fixture
+def tiles(data):
+    """returns a dict linking hashed tile metadata and hashed tile pixels values"""
+    _, hashed = tile_pkl(
+        data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_no_icc.pkl"),
+        data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs"),
+    )
+    return hashed
+
+
+@pytest.fixture
+def it_kwargs_icc(data):
+    """returns a study to generate a tile iterator with icc correction"""
+    kwargs, _ = tile_pkl(
+        data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_icc.pkl"),
+        data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs"),
+    )
+    return kwargs
+
+
+@pytest.fixture
+def it_kwargs(data):
+    """returns a study to generate a tile iterator with icc correction"""
+    kwargs, _ = tile_pkl(
+        data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_no_icc.pkl"),
+        data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs"),
+    )
+    return kwargs
+
+
+def hash_dict(d):
+    """generate a hash from a possibly nested dictionary"""
+
+    def immutify(d):
+        d_new = {}
+        for k, v in d.items():
+            if isinstance(v, np.ndarray):
+                d_new[k] = tuple(v.tolist())
+            elif isinstance(v, list):
+                d_new[k] = tuple(v)
+            elif isinstance(v, dict):
+                d_new[k] = immutify(v)
+            else:
+                # convert numpy types to native
+                if hasattr(v, "dtype"):
+                    d_new[k] = v.item()
+                else:
+                    d_new[k] = v
+        return dict(sorted(d_new.items(), key=lambda item: item[0]))
+
+    d_hashable = immutify(d)
+    s_hashable = json.dumps(d_hashable).encode("utf-8")
+    m = hashlib.sha256(s_hashable).hexdigest()
+    return m
+
+
+def hash_inference(metadata, features):
+    meta_hash = [
+        hash_dict({k: metadata[k][i] for k in HASH_KEYS})
+        for i in range(len(metadata[list(keys)[0]]))
+    ]
+    return {
+        m:f for m,f in zip(meta_hash, features)
+    }
+
+
+@pytest.fixture
+def inference(data):
+    """returns a dict linking hashed tile metadata and inference values"""
+    with open(data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.EfficientNetV2S.tensorflow_224_0_20X.pkl"), "rb") as f:
+        contents = pickle.load(f)
+    return hash_inference(contents["metadata"], contents["features"])
+
+
+def hash_iterator(iterator):
+    """build a dictionary linking tile metadata hashes with tile pixel hashes"""
+    batched = [(i, [{k: s[k] for k in HASH_KEYS} for s in m]) for (i, m) in iterator]
+    linked = {}
+    for batch, metadata in batched:
+        for tile, m in zip(batch, metadata):
+            linked[hash_dict(m)] = hashlib.sha256(tile.tobytes()).hexdigest()
+    return linked
+
+
+def pkl_iterator(path, kwargs):
+    """pickle a hashed version of tile iterator outputs"""
+    hashed = hash_iterator(TiffPrefetch(**kwargs))
+    with open(path, "wb") as f:
+        pickle.dump({"kwargs": kwargs, "hashed": hashed}, f, pickle.HIGHEST_PROTOCOL)
+
--- tests/test_inference.py
+++ tests/test_inference.py
@@ -32,6 +32,51 @@ def python_batch_compare(data, name, max_batch_size=64, rtol=1e-05, atol=1e-08):
     model.unload(block=True)
 
 
+def replace_study_path(study, path):
+    """replace wsi path within study object"""
+    for image in study["slides"].keys():
+        study["slides"][image]["filename"] = path
+    return study
+
+
+def hash_metadata(metadata):
+    keys = {
+        "tile_height",
+        "tile_width",
+        "tile_top",
+        "tile_left",
+        "target_magnification",
+    }
+    return [
+        hash_dict({k: metadata[k][i] for k in keys}) 
+        for i in range(len(metadata[list(keys)[0]]))
+    ]
+
+
+def cosine_similarity(x, y):
+    return np.dot(x, y)/(np.linalg.norm(x)*np.linalg.norm(y))
+
+
+def test_inference(data, triton):
+    with open(data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.hash_icc.pkl"), "rb") as f:
+        kwargs = pickle.load(f)["kwargs"]
+    kwargs["study"] = replace_study_path(kwargs["study"], wsi_path)
+    iterator = TiffPrefetch(**kwargs)
+    features, metadata, times, failures = inference(
+        iterator, "EfficientNetV2S.tensorflow", url="localhost:8001"
+    )
+    inferred = {
+        m:f for m,f in zip(hash_metadata(metadata), np.concatenate(features[0], axis=0))
+    }
+    with open(data.fetch("TCGA-AN-A0G0-01Z-00-DX1.svs.EfficientNetV2S.tensorflow_224_0_20X.pkl"), "rb") as f:
+        contents = pickle.load(f)
+    truth = {
+        m:f for m,f in zip(hash_metadata(contents["metadata"]), contents["features"])
+    }
+    assert truth.keys() == inferred.keys()
+    assert all([cosine_similarity(truth[k], inferred[k]) > 0.999 for k in truth.keys()])
+
+
 def test_hibou_L(data, triton):
     python_batch_compare(data, "hibou-L")
 
