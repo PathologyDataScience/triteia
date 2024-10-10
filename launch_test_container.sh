85ec10f5cfe8aed8b8c13601f76d0b9d6c2f0376
Anders Sildnes <andsild@posteo.net>
2024-10-10 14:45:34 -0500

    Remove temporary directory cleanup

--- launch_test_container.sh
+++ launch_test_container.sh
@@ -11,10 +11,7 @@ if [ -z "${HF_TOKEN}" ]; then
     printf "You can add it by quitting this script/container, and then \`export HF_TOKEN=...\` and re-start the script\n"
 fi
 HF_TOKEN=${HF_TOKEN:-undefined}
-model_tmp_directory="$(mktemp -d -t simple_triton_test_XXXXX)"
+model_tmp_directory=/tmp/simple_triton_test_data_aza4423
+test -d "${model_tmp_directory}" || (mkdir "${model_tmp_directory}" && chmod -R 777 "${model_tmp_directory}")
 cp -r models/* "${model_tmp_directory}"/ || exit 1
-chmod -R 777 "${model_tmp_directory}"
 docker run --security-opt seccomp:unconfined --network=host -v "${model_tmp_directory}:${model_tmp_directory}:rw" -e TRITON_TMP_DIR="${model_tmp_directory}/" -e HF_TOKEN=${HF_TOKEN} --shm-size=2g -v /var/run/docker.sock:/var/run/docker.sock --rm --name tritonclient_test -it simple_triton_client:latest
-# Clean up the temporary directory
-printf "Cleaning up temporary directory: %s\n" "${model_tmp_directory}"
-rm -rf "${model_tmp_directory}" 2>/dev/null || (docker run -v "${model_tmp_directory}:/tmp/" bash:latest bash -c "rm -r /tmp/*" && rmdir "$model_tmp_directory")
