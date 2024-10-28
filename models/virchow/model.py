57b6651654d2356b2f212325ecab5c06adc08197
cooperlab <cooperle@gmail.com>
2024-10-27 19:27:50 -0500

    add virchow model

--- models/virchow/model.py
+++ models/virchow/model.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import os
 from PIL import Image
+import timm
 from timm.data import resolve_data_config
 from timm.data.transforms_factory import create_transform
 from timm.layers import SwiGLUPacked
