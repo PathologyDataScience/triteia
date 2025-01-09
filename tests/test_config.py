ac706658556b1024d57192fb05b7ff74abfbabd5
Anders Sildnes <andsild@posteo.net>
2025-01-09 13:25:38 -0600

    Fix config tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,13 +1,17 @@
 from google.protobuf.json_format import MessageToDict
 import json
 import pytest
-from simple_triton.config import ConfigBuilder
 import tritonclient.grpc as grpcclient
 from tritonclient.utils import InferenceServerException
+import numpy as np
+from .triton import triton
+from .data import data
+
+from simple_triton.config import InstanceGroup, ModelInput, ModelOutput, TensorflowConfig, TensorflowMixedPrecision, TensorflowOptimization, TensorflowXla, TensorRt
+from simple_triton.model import TritonModel
 
 
 MODEL = "EfficientNetV2S.tensorflow"
-URL = "localhost:8001"
 CONFIG = {
     "name": "EfficientNetV2S.tensorflow",
     "platform": "tensorflow_savedmodel",
@@ -35,232 +39,189 @@ CONFIG = {
 BASIC = {"name": MODEL}
 
 
-def test_init():
-    """Verify checking of config type as dict"""
-    with pytest.raises(ValueError):
-        ConfigBuilder(MODEL, config=1)
-
-
-def test_max_batch_size():
+def test_max_batch_size(data, triton):
     """Verify max batch size setting properly"""
-
-    # use fast loading of one instance / one gpu
-    client = grpcclient.InferenceServerClient(url=URL, verbose=False)
+    _, grpc_port, _ = triton
+    url = f"localhost:{grpc_port}"
+    client = grpcclient.InferenceServerClient(url=url, verbose=False)
     batch = 512
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.max_batch_size(batch)
-    builder.remove_instance_groups()
-    builder.add_instance_group(count=1, gpus=[0])
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert MessageToDict(updated)["config"]["maxBatchSize"] == batch
-    with pytest.raises(ValueError):
-        builder.max_batch_size(1.5)
+    model = TritonModel(MODEL, url)
 
+    config = TensorflowConfig(MODEL, max_batch_size=batch)
+    model.load(config=config.json())
+    updated = model.get_config()
+    assert updated["maxBatchSize"] == batch
 
-def test_response_cache():
+    batch = 256
+    config = TensorflowConfig(MODEL, max_batch_size=batch)
+    model.load(config=config.json())
+    updated = model.get_config()
+    assert updated["maxBatchSize"] == batch
+
+def test_response_cache(data, triton):
     """Verify response cache setting properly"""
+    _, grpc_port, _ = triton
+    url = f"localhost:{grpc_port}"
+    client = grpcclient.InferenceServerClient(url=url, verbose=False)
+    model = TritonModel(MODEL, url)
 
-    # use fast loading of one instance / one gpu
-    client = grpcclient.InferenceServerClient(url=URL, verbose=False)
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.response_cache(True)
-    builder.remove_instance_groups()
-    builder.add_instance_group(count=1, gpus=[0])
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert MessageToDict(updated)["config"]["responseCache"] == {"enable": True}
-    builder.response_cache(False)
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert MessageToDict(updated)["config"]["responseCache"] == {}
+    config = TensorflowConfig(MODEL, max_batch_size=128, response_cache=True)
+    model.load(config=config.json())
+    updated = model.get_config()
+    assert updated["responseCache"] == {"enable": True}
+
+    config = TensorflowConfig(MODEL, max_batch_size=128, response_cache=False)
+    model.load(config=config.json())
+    updated = model.get_config()
+    assert updated["responseCache"] == {"enable": False} or updated["responseCache"] == {}
+
+
+def test_add_instance_group(data, triton):
+    """Verify setting instance groups properly"""
+    _, grpc_port, _ = triton
+    url = f"localhost:{grpc_port}"
+    client = grpcclient.InferenceServerClient(url=url, verbose=False)
+    model = TritonModel(MODEL, url)
+
+    instance_group = InstanceGroup(count=0)
+    config = TensorflowConfig(MODEL, max_batch_size=128, instance_group=instance_group)
+    assert "instanceGroup" in config.json()
+    assert not any([x["count"] for x in config.json()["instanceGroup"]])
+
+    instance_group = InstanceGroup(count=2)
+    config = TensorflowConfig(MODEL, max_batch_size=128, instance_group=instance_group)
+    model.load(config=config.json())
+    updated = model.get_config()
+
+    assert updated["instanceGroup"][0]["count"] == 2
+    # skipping this test since it's hardware and triton server specific
+    # but if anyone want to query triton for available GPUS and check that it returns correct results, feel free..
+    # assert updated["instanceGroup"][0]["gpus"] == [0, 1, 2, 3, 4, 5, 6, 7]
+    assert updated["instanceGroup"][0]["kind"] == "KIND_GPU"
+
+
+    instance_group = InstanceGroup(count=1)
+    config = TensorflowConfig(MODEL, max_batch_size=128, instance_group=instance_group)
+    model.load(config=config.json())
+    updated = model.get_config()
+    assert updated["instanceGroup"][0]["count"] == 1
+    assert updated["instanceGroup"][0]["kind"] == "KIND_GPU"
+
+    instance_group = InstanceGroup(count=1, kind="cpu")
+    config = TensorflowConfig(MODEL, max_batch_size=128, instance_group=instance_group)
+    model.load(config=config.json())
+    updated = model.get_config()
+    assert updated["instanceGroup"][0]["count"] == 1
+    assert "gpus" not in updated["instanceGroup"][0]
+
     with pytest.raises(ValueError):
-        builder.response_cache(1.5)
-
-
-def test_add_instance_group():
-    """Verify response cache setting properly"""
-
-    client = grpcclient.InferenceServerClient(url=URL, verbose=False)
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.remove_instance_groups()
-    assert "instanceGroup" not in builder.config
-    builder.add_instance_group(count=2)
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = MessageToDict(client.get_model_config(MODEL))
-    assert updated["config"]["instanceGroup"][0]["count"] == 2
-    assert updated["config"]["instanceGroup"][0]["gpus"] == [0, 1, 2, 3, 4, 5, 6, 7]
-    assert updated["config"]["instanceGroup"][0]["kind"] == "KIND_GPU"
-    builder.remove_instance_groups()
-    builder.add_instance_group(count=1, gpus=[0])
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = MessageToDict(client.get_model_config(MODEL))
-    assert updated["config"]["instanceGroup"][0]["count"] == 1
-    assert updated["config"]["instanceGroup"][0]["gpus"] == [0]
-    assert updated["config"]["instanceGroup"][0]["kind"] == "KIND_GPU"
-    builder.add_instance_group(count=2, kind="cpu")
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = MessageToDict(client.get_model_config(MODEL))
-    assert updated["config"]["instanceGroup"][0]["count"] == 1
-    assert updated["config"]["instanceGroup"][0]["gpus"] == [0]
-    assert updated["config"]["instanceGroup"][0]["kind"] == "KIND_GPU"
-    assert updated["config"]["instanceGroup"][1]["count"] == 2
-    assert updated["config"]["instanceGroup"][1]["kind"] == "KIND_CPU"
+        InstanceGroup(count=1, kind="tpu")
     with pytest.raises(ValueError):
-        builder.add_instance_group(count=1.5)
+        InstanceGroup(count=1, kind="gpu", gpus=["0"])
     with pytest.raises(ValueError):
-        builder.add_instance_group(count=1, kind="tpu")
+        InstanceGroup(count=1, kind="gpu", gpus=["0"])
     with pytest.raises(ValueError):
-        builder.add_instance_group(count=1, kind="gpu", gpus=0)
-    with pytest.raises(ValueError):
-        builder.add_instance_group(count=1, kind="gpu", gpus=["0", "1"])
+        InstanceGroup(count=1, kind="gpu", gpus=["0", "1"])
 
 
-def test_add_mixed_precision():
+def test_add_mixed_precision(data, triton):
     """Verify mixed precision setting properly"""
 
-    # use fast loading of one instance / one gpu
-    client = grpcclient.InferenceServerClient(url=URL, verbose=False)
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.add_mixed_precision()
-    builder.remove_instance_groups()
-    builder.add_instance_group(count=1, gpus=[0])
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert MessageToDict(updated)["config"]["optimization"][
-        "executionAccelerators"
-    ] == {"gpuExecutionAccelerator": [{"name": "auto_mixed_precision"}]}
-    builder.remove_mixed_precision()
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert (
-        "executionAccelerators" not in MessageToDict(updated)["config"]["optimization"]
-    )
-    builder = ConfigBuilder(MODEL, client=client)
-    builder.add_trt("FP16")
-    builder.add_mixed_precision()
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert MessageToDict(updated)["config"]["optimization"][
+    amp = TensorflowMixedPrecision()
+    xla = TensorflowXla(level=2)
+    optimization = TensorflowOptimization(amp=amp, xla=xla)
+    ampxla_config = TensorflowConfig(MODEL, max_batch_size=64, optimization=optimization)
+
+    _, grpc_port, _ = triton
+    url = f"localhost:{grpc_port}"
+    client = grpcclient.InferenceServerClient(url=url, verbose=False)
+    model = TritonModel(MODEL, url)
+
+    model.load(config=ampxla_config.json())
+    updated = model.get_config()
+    assert updated["optimization"][
         "executionAccelerators"
     ] == {"gpuExecutionAccelerator": [{"name": "auto_mixed_precision"}]}
 
 
-def test_add_trt():
-    """Verify mixed precision setting properly"""
-
-    # use fast loading of one instance / one gpu
-    client = grpcclient.InferenceServerClient(url=URL, verbose=False)
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.add_trt("FP16")
-    builder.remove_instance_groups()
-    builder.add_instance_group(count=1, gpus=[0])
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert MessageToDict(updated)["config"]["optimization"][
-        "executionAccelerators"
-    ] == {
-        "gpuExecutionAccelerator": [
-            {"name": "tensorrt", "parameters": {"precision_mode": "FP16"}}
-        ]
-    }
-    builder.add_trt("FP32")
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert MessageToDict(updated)["config"]["optimization"][
-        "executionAccelerators"
-    ] == {
-        "gpuExecutionAccelerator": [
-            {"name": "tensorrt", "parameters": {"precision_mode": "FP32"}}
-        ]
-    }
-    builder.remove_trt()
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
+    client = grpcclient.InferenceServerClient(url=url, verbose=False)
+    batch = 512
+    config = TensorflowConfig(MODEL, max_batch_size=batch)
+    model.load(config=config.json())
+    updated = model.get_config()
     assert (
-        "executionAccelerators" not in MessageToDict(updated)["config"]["optimization"]
+        "executionAccelerators" not in updated["optimization"]
     )
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.add_mixed_precision()
-    builder.add_trt("FP16")
-    client.load_model(MODEL, config=json.dumps(builder.config))
-    updated = client.get_model_config(MODEL)
-    assert MessageToDict(updated)["config"]["optimization"][
-        "executionAccelerators"
-    ] == {
-        "gpuExecutionAccelerator": [
-            {"name": "tensorrt", "parameters": {"precision_mode": "FP16"}}
-        ]
-    }
+
     with pytest.raises(ValueError):
-        builder.add_trt("half-float")
+        TensorflowOptimization(amp=amp, trt=TensorRt(precision_mode="FP16"))
+
+
+def test_add_trt(data, triton):
+    """Verify trt setting properly"""
+    _, grpc_port, _ = triton
+    url = f"localhost:{grpc_port}"
+    client = grpcclient.InferenceServerClient(url=url, verbose=False)
+    model = TritonModel(MODEL, url)
+
+
+    def test_fp(precision_mode):
+        trt = TensorRt(precision_mode=precision_mode)
+        optimization = TensorflowOptimization(trt=trt)
+        config = TensorflowConfig(MODEL, max_batch_size=64, optimization=optimization)
+        model.load(config=config.json())
+        updated = model.get_config()["optimization"]
+        assert "executionAccelerators" in updated and "gpuExecutionAccelerator" in updated["executionAccelerators"]
+        updated = updated["executionAccelerators"]['gpuExecutionAccelerator'][0]
+        assert "name" in updated and "parameters" in updated and "precision_mode" in updated["parameters"]
+        assert updated["name"] == "tensorrt"
+        assert updated["parameters"]["precision_mode"] == precision_mode
+
+    test_fp("FP16")
+    test_fp("FP32")
+
+    config = TensorflowConfig(MODEL, max_batch_size=64, optimization=None)
+    model.load(config=config.json())
+    updated = model.get_config()["optimization"]
+    assert "optimization" not in updated
+
+    test_fp("FP16")
+
+    with pytest.raises(ValueError):
+        trt = TensorRt(precision_mode="half-float")
 
 
 def test_add_input():
     """Verify input setting properly."""
+    inputs = ModelInput(name ="input_1", shape=[-1, -1, 3], dtype=np.float32)
+    config = TensorflowConfig(MODEL, max_batch_size=128, input=inputs)
 
-    # offline comparison of config
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.add_input("input_1", "TYPE_FP32", [-1, -1, 3])
-    assert builder.config["input"] == [
+    assert config.json()["input"] == [
         {"name": "input_1", "dataType": "TYPE_FP32", "dims": [-1, -1, 3]}
     ]
-    builder.add_input("input_1", "TYPE_FP32", [224, 224, 3])
-    assert builder.config["input"] == [
+
+    inputs = ModelInput(name ="input_1", shape=[224, 224, 3], dtype=np.float32)
+    config = TensorflowConfig(MODEL, max_batch_size=128, input=inputs)
+    assert config.json()["input"] == [
         {"name": "input_1", "dataType": "TYPE_FP32", "dims": [224, 224, 3]}
     ]
-    builder.add_input("input_2", "TYPE_FP16", [224, 224, 3])
-    assert builder.config["input"] == [
-        {"name": "input_1", "dataType": "TYPE_FP32", "dims": [224, 224, 3]},
-        {"name": "input_2", "dataType": "TYPE_FP16", "dims": [224, 224, 3]},
-    ]
-    builder.config["input"] = [
-        {"name": "input_2", "dataType": "TYPE_FP32", "dims": ["224", "224", "3"]}
-    ]
 
-
-def test_remove_inputs():
-    """Verify removal of inputs"""
-
-    # offline comparison of config
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.remove_inputs()
-    assert "input" not in builder.config.keys()
-    builder.config["input"] = [
-        {"name": "input_2", "dataType": "TYPE_FP32", "dims": ["224", "224", "3"]}
+    inputs = ModelInput(name ="input_2", shape=[224, 224, 3], dtype=np.float32)
+    config = TensorflowConfig(MODEL, max_batch_size=128, input=inputs)
+    assert config.json()["input"] == [
+        {"name": "input_2", "dataType": "TYPE_FP32", "dims": [224, 224, 3]}
     ]
 
 
 def test_add_output():
     """Verify output setting properly."""
+    outputs = ModelOutput(name = "output_1", shape = [1280], dtype=np.float32)
+    config = TensorflowConfig(MODEL, max_batch_size=128, output=outputs).json()
+    assert "output" in config and len(config["output"]) == 1, config
+    assert config["output"][0] == {"name": "output_1", "dataType": "TYPE_FP32", "dims": [1280]}, config["output"][0]
 
-    # offline comparison of config
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.add_output("output_1", "TYPE_FP32", [1280])
-    assert builder.config["output"] == [
-        {"name": "output_1", "dataType": "TYPE_FP32", "dims": [1280]}
-    ]
-    builder.add_output("output_1", "TYPE_FP32", [1281])
-    assert builder.config["output"] == [
-        {"name": "output_1", "dataType": "TYPE_FP32", "dims": [1281]}
-    ]
-    builder.add_output("output_2", "TYPE_FP16", [4])
-    assert builder.config["output"] == [
-        {"name": "output_1", "dataType": "TYPE_FP32", "dims": [1281]},
-        {"name": "output_2", "dataType": "TYPE_FP16", "dims": [4]},
-    ]
-    builder.config["output"] = [
-        {"name": "avg_pool", "dataType": "TYPE_FP32", "dims": ["1280"]}
-    ]
+    outputs = ModelOutput(name = "output_2", shape = [4], dtype=np.float16)
+    config = TensorflowConfig(MODEL, max_batch_size=128, output=outputs).json()
+    assert "output" in config and len(config["output"]) == 1, config
+    assert config["output"][0] == {"name": "output_2", "dataType": "TYPE_FP16", "dims": [4]}, config["output"][0]
 
-
-def test_remove_outputs():
-    """Verify removal of outputs"""
-
-    # offline comparison of config
-    builder = ConfigBuilder(MODEL, config=BASIC)
-    builder.remove_outputs()
-    assert "output" not in builder.config.keys()
-    builder.config["output"] = [
-        {"name": "avg_pool", "dataType": "TYPE_FP32", "dims": ["1280"]}
-    ]
