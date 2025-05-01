4e9a45e8c296c57da45731705cc9422778db49e5
cooperlab <cooperle@gmail.com>
2025-04-30 20:30:10 -0500

    simplify server container setup

--- setup_repository.py
+++ setup_repository.py
@@ -4,6 +4,8 @@ import os
 import shutil
 import sys
 
+MODEL_ROOT = "/hosted"
+
 
 def model_setup(source, destination):
     """Creates a valid model folder at destination"""
@@ -18,16 +20,15 @@ def main():
     if not len(sys.argv) == 2:
         raise TypeError(f"setup_repository.py takes 1 argument for destination path.")
     move = []
-    model_root = os.path.join(os.path.dirname(__file__), "models")
-    for f in os.listdir(model_root):
-        if os.path.isdir(os.path.join(model_root, f)):
-            if os.listdir(os.path.join(model_root, f)) == ["model.py"]:
+    for f in os.listdir(MODEL_ROOT):
+        if os.path.isdir(os.path.join(MODEL_ROOT, f)):
+            if os.listdir(os.path.join(MODEL_ROOT, f)) == ["model.py"]:
                 move.append(f)
     if len(move):
         print(f"Found {len(move)} models.")
         for f in move:
             print(f" moving {f}")
-            model_setup(os.path.join(model_root, f), sys.argv[1])
+            model_setup(os.path.join(MODEL_ROOT, f), sys.argv[1])
         print(f"Models setup in {sys.argv[1]}.")
     else:
         print(f"No model folders found in {model_root}")
